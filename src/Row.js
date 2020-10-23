79f566f5e058caab315c58c55cd8c935431dae0f
AzatErnis <azat.ernis@gmail.com>
2020-10-23 20:38:28 +0600

    add Row.css, need to fix overflow issue, add requests.js

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from './axios'
+import './Row.css'
 
-const Row = () => {
+const baseUrl = 'https://image.tmdb.org/t/p/original'
+
+const Row = ({ title, fetchUrl }) => {
+
+  const [movies, setMovies] = useState([])
+
+  // A snippet of code which runs based on a specific condition
+  useEffect(() => {
+    // if [], run once when the row loads, and dont run again, if we pull f.e. [movies], it's called depensensies and it will reloaded everytime when movies changed
+    const fetchDate = async () => {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results);
+      return request
+    }
+    fetchDate()
+  }, [fetchUrl])
+
+  console.log(movies);
+
   return (
-    <div>
-
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className="row__posts">
+        {
+          movies.map(movie => (
+            <img className='row__posts-img' src={`${baseUrl}${movie.poster_path}`} alt={movie.name} />
+          ))
+        }
+      </div>
+      {/* container -> posters */}
     </div>
   )
 }
