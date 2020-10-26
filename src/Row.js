b5290f8804853b5f999a2bab1e328b9457dab9c4
AzatErnis <azat.ernis@gmail.com>
2020-10-26 23:00:54 +0600

    added new functionality to watch trailer from YouTube

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 import './Row.css'
 
 const baseUrl = 'https://image.tmdb.org/t/p/original'
@@ -7,6 +9,7 @@ const baseUrl = 'https://image.tmdb.org/t/p/original'
 const Row = ({ title, fetchUrl, isLarge }) => {
 
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState("")
 
   // A snippet of code which runs based on a specific condition
   useEffect(() => {
@@ -19,6 +22,28 @@ const Row = ({ title, fetchUrl, isLarge }) => {
     fetchDate()
   }, [fetchUrl])
 
+  console.log(movies)
+
+  const opts = {
+    height: "500",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = movie => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie.original_name || "")
+        .then(url => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get('v'))
+        }).catch(err => console.log(err))
+    }
+  }
+
   return (
     <div className='row'>
       <h2>{title}</h2>
@@ -26,15 +51,16 @@ const Row = ({ title, fetchUrl, isLarge }) => {
         {
           movies.map(movie => (
             <img
-              key={movie.id}
               className={`row__poster ${isLarge && 'row__poster--large'}`}
+              onClick={() => handleClick(movie)}
+              key={movie.id}
               src={`${baseUrl}${isLarge ? movie.poster_path : movie.backdrop_path}`}
               alt={movie.name}
             />
           ))
         }
       </div>
-      {/* container -> posters */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
